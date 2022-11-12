27f77d07714e333029bc172adf14186639dbb643
Muhammad Hassan <786hassan777@gmail.com>
2022-11-12 12:04:35 +0500

    Update

--- hoppe.py
+++ hoppe.py
@@ -1,4 +1,6 @@
 import json
+import os
+from urllib.parse import unquote
 
 import requests
 from bs4 import BeautifulSoup
@@ -34,12 +36,93 @@ def getData(url):
         litxt = li.text.strip().split(':')
         data[litxt[0].strip()] = litxt[1].strip()
     print(json.dumps(data, indent=4))
+    row = {
+        "SKU": data['SKU'],
+        "Type": "simple",
+        "Parent": "",
+        "Name": data['Name'],
+        "Price": data['Price'],
+        "Categories": data['Categories'],
+        "Images": data['Images'],
+        "Attribute 1 Name": "Finish",
+        "Attribute 1 Value(s)": data['Finish'],
+        "Attribute 1 Global": "0",
+        "Attribute 1 Visible": "1",
+        "Attribute 2 Name": "Version",
+        "Attribute 2 Value(s)": data['Version'],
+        "Attribute 2 Global": "0",
+        "Attribute 2 Visible": "1",
+        "Description": data['Description'],
+    }
+    print(json.dumps(row, indent=4))
     return data
 
 
+def processCategory(url):
+    print(f"Category {url}")
+    soup = BeautifulSoup(requests.get(url).text, 'lxml')
+    while url:
+        for div in soup.find_all('h2', {'class': 'product-name'}):
+            if div.find('a'):
+                href = div.find('a')['href']
+
+                file = f"./hoppe/{href.split('/')[-2]}.html"
+                if not os.path.exists(file):
+                    print(f"Working on {href}")
+                    res = BeautifulSoup(requests.get(href).text, 'lxml')
+                    with open(file, 'w') as hfile:
+                        hfile.write(res.prettify())
+                else:
+                    print(f"Already scraped {href}")
+        page_next = soup.find('li', {"class": "pagination-next"})
+        if page_next:
+            page_next = page_next.find('a')
+            if page_next:
+                url = f"https://www.hoppe.com{page_next['href']}"
+            else:
+                break
+        else:
+            break
+        print(f"Next page {url}")
+        soup = BeautifulSoup(requests.get(url).text, 'lxml')
+
+
+def scrapeAllProducts():
+    url = 'https://www.hoppe.com/gb-en/product-catalogue/'
+    # with open('hoppe-prod.html') as hfile:
+    #     soup = BeautifulSoup(hfile, 'lxml')
+    soup = BeautifulSoup(requests.get(url).text, 'lxml')
+    x = "var wc_product_block_data = JSON.parse( decodeURIComponent( '"
+    products = {}
+    for line in soup.prettify().splitlines():
+        if x in line:
+            products = json.loads(unquote(line.replace(x, '')[:-6].strip()))
+    for cat in products['productCategories']:
+        processCategory(cat['link'])
+
+
 def main():
-    url = 'https://www.hoppe.com/gb-en/product/amsterdam-handle-sets-for-interior-doors-1001192940-1001192940/'
-    getData(url)
+    logo()
+    if not os.path.exists('./hoppe'):
+        os.mkdir('./hoppe')
+    scrapeAllProducts()
+
+
+def logo():
+    print(r"""
+      ___ ___                              
+     /   |   \  ____ ______ ______   ____  
+    /    ~    \/  _ \\____ \\____ \_/ __ \ 
+    \    Y    (  <_> )  |_> >  |_> >  ___/ 
+     \___|_  / \____/|   __/|   __/ \___  >
+           \/        |__|   |__|        \/ 
+================================================
+        Hoppe Scraper by @evilgenius786
+================================================
+[+] Scraping all products from Hoppe
+[+] CSV/JSON Output
+________________________________________________
+""")
 
 
 if __name__ == '__main__':
